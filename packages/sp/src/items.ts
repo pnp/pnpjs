d1ad078ddda80ea4f1d549038887d99c5c6e34f0
Patrick Rodgers <patrodg@microsoft.com>
2018-08-23 12:50:45 -0400

    move accept header in getAll to parameter

--- packages/sp/src/items.ts
+++ packages/sp/src/items.ts
@@ -58,6 +58,7 @@ export class Items extends SharePointQueryableCollection {
      * Gets all the items in a list, regardless of count. Does not support batching or caching
      *
      *  @param requestSize Number of items to return in each request (Default: 2000)
+     *  @param acceptHeader Allows for setting the value of the Accept header for SP 2013 support
      */
     public getAll(requestSize = 2000, acceptHeader = "application/json;odata=nometadata"): Promise<any[]> {
 
