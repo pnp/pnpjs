8df17981c9bc3cab70b23d601e62c2d70bc11a6a
Patrick Rodgers <patrodg@microsoft.com>
2018-11-20 12:12:58 -0500

    Update nodejs.ts

--- packages/nodejs/src/nodejs.ts
+++ packages/nodejs/src/nodejs.ts
@@ -21,4 +21,4 @@ const NodeFetch = require("node-fetch");
 export * from "./net/sharepoint/index";
 export { AdalFetchClient, AADToken } from "./net/adal/adalfetchclient";
 export { NodeFetchClient } from "./net/nodefetchclient";
-export { BearerTokenFetchClient } from "./net/BearerTokenFetchClient";
+export { BearerTokenFetchClient } from "./net/bearertokenfetchclient";
