26ed8c001db08bf79ee858b2355c0a7b646d1007
Nanddeep Nachan <nanddeep_nachan@hotmail.com>
2019-05-12 10:06:48 +0000

    Added test getByTitle

--- packages/sp/__tests/views.test.ts
+++ packages/sp/__tests/views.test.ts
@@ -21,4 +21,12 @@ describe("Views", () => {
                 .to.match(toMatchEndRegex("_api/web/lists/getByTitle('Tasks')/views('7b7c777e-b749-4f58-a825-53084f2941b0')"));
         });
     });
+    describe("getByTitle", () => {
+        it("Should return /_api/web/lists/getByTitle('Tasks')/views/getbytitle('All%20Tasks')", () => {
+            const views = new Views("_api/web/lists/getByTitle('Tasks')");
+            const view = views.getByTitle("All Tasks");
+            expect(view.toUrl())
+                .to.match(toMatchEndRegex("/_api/web/lists/getByTitle('Tasks')/views/getbytitle('All%20Tasks')"));
+        });
+    });
 });
