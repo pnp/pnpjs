908554d579617ffebefd379763cc5595047d13a8
ARVO\andrew.koltyakov <andrew.koltyakov@arvosys.com>
2019-05-30 17:01:00 +0300

    polyfill-ie11: object assign, fix #714

--- tools/polyfill-ie11/index.ts
+++ tools/polyfill-ie11/index.ts
@@ -1,6 +1,7 @@
 import "core-js/modules/es6.promise";
-import "core-js/modules/es6.array.iterator.js";
-import "core-js/modules/es6.array.from.js";
-import "core-js/modules/es6.array.fill.js";
+import "core-js/modules/es6.array.iterator.js.js";
+import "core-js/modules/es6.array.from.js.js";
+import "core-js/modules/es6.array.fill.js.js";
+import "core-js/stable/object/assign";
 import "es6-map/implement";
 import "whatwg-fetch";
