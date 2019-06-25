452fd25899e31748320b35418a72fa9b1cb866de
Patrick Rodgers <patrodg@microsoft.com>
2019-06-25 16:22:12 -0400

    updating polyfill package

--- tools/polyfill-ie11/index.ts
+++ tools/polyfill-ie11/index.ts
@@ -1,7 +1,7 @@
 import "core-js/modules/es6.promise";
-import "core-js/modules/es6.array.iterator.js.js";
-import "core-js/modules/es6.array.from.js.js";
-import "core-js/modules/es6.array.fill.js.js";
-import "core-js/stable/object/assign";
+import "core-js/modules/es6.array.iterator";
+import "core-js/modules/es6.array.from";
+import "core-js/modules/es6.array.fill";
+import "core-js/modules/es6.object.assign";
 import "es6-map/implement";
 import "whatwg-fetch";
