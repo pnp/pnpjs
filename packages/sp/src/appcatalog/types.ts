899650e39c54e6084a6189952edb3a711be3ca9b
Kemal S <kemicza@gmail.com>
2019-07-31 14:31:25 +0200

    completed tests & documentation for appcatalog v2

--- packages/sp/src/appcatalog/types.ts
+++ packages/sp/src/appcatalog/types.ts
@@ -11,6 +11,8 @@ import { spPost } from "../operations";
 import { odataUrlFrom } from "../odata";
 import { extractWebUrl } from "../utils/extractweburl";
 import { File, IFile } from "../files/types";
+import { Web } from '../webs';
+import '../items';
 
 export class _AppCatalog extends _SharePointQueryableCollection implements _IAppCatalog {
 
@@ -22,16 +24,37 @@ export class _AppCatalog extends _SharePointQueryableCollection implements _IApp
         return App(this, `getById('${id}')`);
     }
 
-    public async add(filename: string, content: any, shouldOverWrite = true): Promise<IAppAddResult> {
+    public async syncSolutionToTeams(id: string | number, useSharePointItemId: boolean = false): Promise<void> {
+        // This REST call requires that you refer the list item id of the solution in the app catalog site.
+        let appId = null;
+        const webUrl = extractWebUrl(this.toUrl());
+
+        if (useSharePointItemId === true) {
+            appId = id;
+        }
+        else {
+            const web = Web(webUrl);
+            const listId = (await web.lists.select("Id").filter(`EntityTypeName eq 'AppCatalog'`).get())[0].Id;
+            const listItems = await web.lists.getById(listId).items.filter(`AppProductID eq '${id}'`).top(1).get();
+            if (listItems && listItems.length > 0) {
+                appId = listItems[0].Id;
+            }
+            else {
+                throw Error(`Did not find the app with id ${id} in the appcatalog (╯°□°）╯︵ ┻━┻`);
+            }
+        }
+
+        const poster = AppCatalog(webUrl, `_api/web/tenantappcatalog/SyncSolutionToTeams(id=${appId})`);
+        return await spPost(poster, {});
+    }
+
+    public async add(filename: string, content: string | ArrayBuffer | Blob, shouldOverWrite = true): Promise<IAppAddResult> {
         // you don't add to the availableapps collection
         const adder = AppCatalog(extractWebUrl(this.toUrl()), `_api/web/tenantappcatalog/add(overwrite=${shouldOverWrite},url='${filename}')`);
 
         const r = await spPost(adder, {
             body: content, headers: {
                 "binaryStringRequestBody": "true",
-                "accept": "application/json;odata=verbose",
-                "content-type": "application/json;odata=verbose",
-                "content-length": "3521",
             }
         });
 
@@ -54,12 +77,18 @@ export interface _IAppCatalog {
      * @param shouldOverWrite Should an app with the same name in the same location be overwritten? (default: true)
      * @returns Promise<AppAddResult>
      */
-    add(filename: string, content: any, shouldOverWrite?: boolean): Promise<IAppAddResult>;
+    add(filename: string, content: string | ArrayBuffer | Blob, shouldOverWrite?: boolean): Promise<IAppAddResult>;
     /**
      * Get details of specific app from the app catalog
      * @param id - Specify the guid of the app
      */
     getAppById(id: string): IApp;
+    /**
+     * Synchronize a solution to the Microsoft Teams App Catalog
+     * @param id - Specify the guid of the app
+     * @param useSharePointItemId (optional) - By default this REST call requires the SP Item id of the app, not the app id. PnPjs will try to fetch the item id by default, you can still use this parameter to pass your own item id in the first parameter 
+     */
+    syncSolutionToTeams(id: string | number, useSharePointItemId?: boolean): Promise<void>;
 }
 
 export interface IAppCatalog extends _IAppCatalog, IInvokable, ISharePointQueryableCollection { }
@@ -115,6 +144,7 @@ export interface _IApp {
     install(): Promise<void>;
     /**
      * This method allows an app which is already installed to be uninstalled on a web
+     * Note: when you use the REST API to uninstall a solution package from the site, it is not relocated to the recycle bin
      */
     uninstall(): Promise<void>;
     /**
@@ -138,7 +168,12 @@ export interface IApp extends _IApp, IInvokable, ISharePointQueryableInstance { 
  */
 export const App = spInvokableFactory<IApp>(_App);
 
+/**
+ * Result object after adding an app
+ */
 export interface IAppAddResult {
+    // Contains metadata of the added app
     data: any;
+    // A File instance to the item in SharePoint
     file: IFile;
 }
