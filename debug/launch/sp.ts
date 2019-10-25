b8aa83c620993076a9ba18d2714cd6898e368cc7
ValerasNarbutas <valerasn@gmail.com>
2019-10-25 11:50:38 +0300

    rollback

--- debug/launch/sp.ts
+++ debug/launch/sp.ts
@@ -1,4 +1,5 @@
 import { SPFetchClient } from "@pnp/nodejs";
+
 import { sp } from "@pnp/sp";
 import "@pnp/sp/src/webs";
 <<<<<<< HEAD
@@ -7,8 +8,11 @@ import "@pnp/sp/src/site-users/web";
 import "@pnp/sp/src/sharing/web";
 import { SharingLinkKind } from "@pnp/sp/src/sharing";
 import { dateAdd } from "@pnp/common";
+<<<<<<< HEAD
 import {  ISiteUserProps } from "@pnp/sp/src/site-users/types";
 >>>>>>> Adding Documentation for site Users
+=======
+>>>>>>> rollback
 
 declare var process: { exit(code?: number): void };
 
@@ -23,17 +27,21 @@ export async function Example(settings: any) {
     },
   });
 <<<<<<< HEAD
+<<<<<<< HEAD
 
   const r = await sp.web();
 =======
   let user: ISiteUserProps;
    user = await sp.web.siteUsers.getByEmail("valeras.narbutas@macaw.nl")();
+=======
+
+  const user = await sp.web.siteUsers.getByEmail("patrick@three18studios.com")();
+
+>>>>>>> rollback
   const r = await sp.web.shareWith(user.LoginName);
 >>>>>>> Adding Documentation for site Users
 
   console.log(JSON.stringify(r, null, 2));
-  console.log(user.Title);
-  console.log(user.Title);
 
   process.exit();
 }
