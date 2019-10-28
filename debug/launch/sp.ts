e3d4201be5b204c12d5c8213f784f08ddaefad7c
Valeras Narbutas <valerasn@gmail.com>
2019-10-28 23:16:13 +0200

    test

--- debug/launch/sp.ts
+++ debug/launch/sp.ts
@@ -1,18 +1,9 @@
 import { SPFetchClient } from "@pnp/nodejs";
-
 import { sp } from "@pnp/sp";
 import "@pnp/sp/src/webs";
-<<<<<<< HEAD
-=======
 import "@pnp/sp/src/site-users/web";
 import "@pnp/sp/src/sharing/web";
-import { SharingLinkKind } from "@pnp/sp/src/sharing";
-import { dateAdd } from "@pnp/common";
-<<<<<<< HEAD
-import {  ISiteUserProps } from "@pnp/sp/src/site-users/types";
->>>>>>> Adding Documentation for site Users
-=======
->>>>>>> rollback
+
 
 declare var process: { exit(code?: number): void };
 
@@ -26,20 +17,13 @@ export async function Example(settings: any) {
       },
     },
   });
-<<<<<<< HEAD
-<<<<<<< HEAD
+
+
 
   const r = await sp.web();
-=======
-  let user: ISiteUserProps;
-   user = await sp.web.siteUsers.getByEmail("valeras.narbutas@macaw.nl")();
-=======
 
   const user = await sp.web.siteUsers.getByEmail("patrick@three18studios.com")();
 
->>>>>>> rollback
-  const r = await sp.web.shareWith(user.LoginName);
->>>>>>> Adding Documentation for site Users
 
   console.log(JSON.stringify(r, null, 2));
 
