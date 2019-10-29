f879758fdad17a8dc2dab23e62c9afaeaf2b1031
Patrick Rodgers <patrodg@microsoft.com>
2019-10-28 21:44:30 -0400

    fixing build error

--- packages/config-store/providers/spListConfigurationProvider.ts
+++ packages/config-store/providers/spListConfigurationProvider.ts
@@ -2,6 +2,8 @@ import { IConfigurationProvider } from "../configuration";
 import { TypedHash } from "@pnp/common";
 import { default as CachingConfigurationProvider } from "./cachingConfigurationProvider";
 import { IWeb } from "@pnp/sp/webs";
+import "@pnp/sp/lists/web";
+import "@pnp/sp/items/list";
 
 /**
  * A configuration provider which loads configuration values from a SharePoint list
