8e6b7e338ad2a3aec52e84a1b8128bb80e015932
Kemal S <kemicza@gmail.com>
2019-10-30 13:26:02 +0100

    ICreateWikiPageResult docs typo

--- debug/launch/sp.ts
+++ debug/launch/sp.ts
@@ -2,9 +2,6 @@ import { SPFetchClient } from "@pnp/nodejs";
 
 import { sp } from "@pnp/sp";
 import "@pnp/sp/webs";
-import { sp } from "@pnp/sp";
-import "@pnp/sp/sputilities";
-import { ICreateWikiPageResult } from "@pnp/sp/sputilities";
 
 declare var process: { exit(code?: number): void };
 
@@ -19,11 +16,9 @@ export async function Example(settings: any) {
     },
   });
 
+  const r = await sp.web();
 
-  let newPage : ICreateWikiPageResult = await sp.utility.createWikiPage({
-      ServerRelativeUrl: "/sites/dev/SitePages/mynewpage.aspx",
-      WikiHtmlContent: "This is my <b>page</b> content. It supports rich html.",
-  });
+  console.log(JSON.stringify(r, null, 2));
 
   process.exit();
 }
