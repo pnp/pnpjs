dac94aea1129c43979bd4d32046b8905cb042438
Kemal S <kemicza@gmail.com>
2019-10-30 13:25:17 +0100

    sp.ts restore

--- debug/launch/sp.ts
+++ debug/launch/sp.ts
@@ -2,6 +2,9 @@ import { SPFetchClient } from "@pnp/nodejs";
 
 import { sp } from "@pnp/sp";
 import "@pnp/sp/webs";
+import { sp } from "@pnp/sp";
+import "@pnp/sp/sputilities";
+import { ICreateWikiPageResult } from "@pnp/sp/sputilities";
 
 declare var process: { exit(code?: number): void };
 
@@ -16,9 +19,11 @@ export async function Example(settings: any) {
     },
   });
 
-  const r = await sp.web();
 
-  console.log(JSON.stringify(r, null, 2));
+  let newPage : ICreateWikiPageResult = await sp.utility.createWikiPage({
+      ServerRelativeUrl: "/sites/dev/SitePages/mynewpage.aspx",
+      WikiHtmlContent: "This is my <b>page</b> content. It supports rich html.",
+  });
 
   process.exit();
 }
--- docs/sp/sp-utilities-utility.md
+++ docs/sp/sp-utilities-utility.md
@@ -127,7 +127,7 @@ import { sp } from "@pnp/sp";
 import "@pnp/sp/sputilities";
 import { ICreateWikiPageResult } from "@pnp/sp/sputilities";
 
-let newPage : CreateWikiPageResult = await sp.utility.createWikiPage({
+let newPage : ICreateWikiPageResult = await sp.utility.createWikiPage({
     ServerRelativeUrl: "/sites/dev/SitePages/mynewpage.aspx",
     WikiHtmlContent: "This is my <b>page</b> content. It supports rich html.",
 });
