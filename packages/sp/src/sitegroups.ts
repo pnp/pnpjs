b4ed7776231295ebdd3e1c910de782c5828b3c5f
Kemal S <kemicza@gmail.com>
2019-12-17 10:09:14 +0100

    setUserAsOwner & setGroupAsOwner on SiteGroup

--- packages/sp/src/sitegroups.ts
+++ packages/sp/src/sitegroups.ts
@@ -2,6 +2,9 @@ import { SharePointQueryableInstance, SharePointQueryableCollection, defaultPath
 import { SiteUsers } from "./siteusers";
 import { extend, TypedHash, jsS, hOP } from "@pnp/common";
 import { metadata } from "./utils/metadata";
+import { extractWebUrl } from "./utils/extractweburl";
+import { Site } from "..";
+import { ClientSvcQueryable } from "@pnp/sp-clientsvc";
 
 /**
  * Principal Type enum
@@ -123,6 +126,49 @@ export class SiteGroup extends SharePointQueryableInstance {
             group: retGroup,
         };
     });
+
+    /**
+     * Set the owner of a group using a user id
+     * @param userId the id of the user that will be set as the owner of the current group
+     */
+    public async setUserAsOwner(userId: number): Promise<any> {
+
+        return this.clone(SiteGroup, `SetUserAsOwner(${userId})`).postCore();
+    }
+
+    /**
+     * Set the owner of a group using a group id (Uses ProcessQuery)
+     * @param groupId the id of the group that will be set as the owner of the current group
+     */
+    public async setGroupAsOwner(groupId: number): Promise<any> {
+
+        const group = await this.clone(SiteGroup).select("Id").get();
+        const site = await new Site(extractWebUrl(this.toUrl())).select("Id").get();
+        return new SiteGroupSvc().setGroupAsOwner(groupId, group.Id, site.Id);
+    }
+}
+
+class SiteGroupSvc extends ClientSvcQueryable {
+
+    public setGroupAsOwner(newGroupOwnerId: number, groupId: number, siteId: number): Promise<any> {
+
+        const body = `<Request AddExpandoFieldTypeSuffix="true" SchemaVersion="15.0.0.0" LibraryVersion="15.0.0.0" ApplicationName=".NET Library" xmlns="http://schemas.microsoft.com/sharepoint/clientquery/2009">
+            <Actions>
+              <SetProperty Id="1" ObjectPathId="2" Name="Owner">
+                <Parameter ObjectPathId="3" />
+              </SetProperty>
+              <Method Name="Update" Id="4" ObjectPathId="2" />
+            </Actions>
+            <ObjectPaths>
+              <Identity Id="2" Name="740c6a0b-85e2-48a0-a494-e0f1759d4aa7:site:${siteId}:g:${groupId}" />
+              <Identity Id="3" Name="740c6a0b-85e2-48a0-a494-e0f1759d4aa7:site:${siteId}:g:${newGroupOwnerId}" />
+            </ObjectPaths>
+          </Request>`;
+
+        return this.postCore({
+            body: body,
+        });
+    }
 }
 
 export interface SiteGroupAddResult {
