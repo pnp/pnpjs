657836246ae9826bb6ae218823ff82630e15de1f
patrick-rodgers <patrick.rodgers@microsoft.com>
2021-01-05 11:35:43 -0500

    fixing debug file

--- debug/launch/sp.ts
+++ debug/launch/sp.ts
@@ -3,9 +3,6 @@ import { Logger, LogLevel } from "@pnp/logging";
 import { sp } from "@pnp/sp";
 import { spSetup } from "./setup.js";
 import "@pnp/sp/webs";
-import "@pnp/sp/lists";
-import "@pnp/sp/items";
-import { getRandomString } from "@pnp/common";
 
 declare var process: { exit(code?: number): void };
 
@@ -13,29 +10,13 @@ export async function Example(settings: ITestingSettings) {
 
   spSetup(settings);
 
-  const items = sp.web.lists.getByTitle("a new list").items;
+  const w = sp.web();
 
-await items.add({
-    Title: `t-${getRandomString(4)}`
+  Logger.log({
+    data: w,
+    level: LogLevel.Info,
+    message: "List of Web Data",
   });
-
-  await items.add({
-    Title: `t-${getRandomString(4)}`
-  });
-
-  await items.add({
-    Title: `t-${getRandomString(4)}`
-  });
-
-  await items.add({
-    Title: `t-${getRandomString(4)}`
-  });
-
-  // Logger.log({
-  //   data: w,
-  //   level: LogLevel.Info,
-  //   message: "List of Web Data",
-  // });
 
   process.exit(0);
 }
