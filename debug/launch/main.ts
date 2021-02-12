c80f96cb9040c00f551b4e720aff00478b2034fe
patrick-rodgers <patrick.rodgers@microsoft.com>
2021-02-12 09:32:52 -0500

    fix for #1572, throw error for root folder getItem()

--- debug/launch/main.ts
+++ debug/launch/main.ts
@@ -8,7 +8,7 @@ import { ITestingSettings } from "../../test/settings.js";
 // add your debugging imports here and prior to submitting a PR git checkout debug/debug.ts
 // will allow you to keep all your debugging files locally
 // comment out the example
-import { Example } from "./taxonomy.js";
+import { Example } from "./sp.js";
 
 // setup the connection to SharePoint using the settings file, you can
 // override any of the values as you want here, just be sure not to commit
