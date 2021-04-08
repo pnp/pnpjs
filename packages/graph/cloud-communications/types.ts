c57d6047c1231e318890526cbf8ccbdeeffa7c82
Julie Turner <julie@julieturner.net>
2021-04-08 13:21:50 -0400

    Add Graph User Presence

---
+++ packages/graph/cloud-communications/types.ts
@@ -0,0 +1,31 @@
+import { body } from "@pnp/odata";
+import { Presence as IUserPresence } from "@microsoft/microsoft-graph-types";
+import { _GraphQueryableCollection, graphInvokableFactory, _GraphQueryableInstance } from "../graphqueryable.js";
+import { defaultPath } from "../decorators.js";
+import { graphPost } from "../operations.js";
+
+/**
+ * Presence
+ */
+
+@defaultPath("presence")
+export class _Presence extends _GraphQueryableInstance<IUserPresence> { }
+export interface IPresence extends _Presence { }
+export const Presence = graphInvokableFactory<IPresence>(_Presence);
+
+@defaultPath("communications")
+export class _Communications extends _GraphQueryableCollection<IUserPresence[]> {
+    /**
+     * Retrieve presence information for a group of users.
+     *
+     * @param ids An array of user id's to retrieve presence for.
+     */
+    public async getPresencesByUserId(ids: string[]): Promise<IUserPresence[]> {
+
+        const postBody = { ids };
+
+        return graphPost<IUserPresence[]>(this.clone(Communications, "getPresencesByUserId"), body(postBody));;
+    }
+}
+export interface ICommunications extends _Communications { }
+export const Communications = graphInvokableFactory<ICommunications>(_Communications);
