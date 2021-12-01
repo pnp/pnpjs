d28112118d16f4253e36ec92b54ea8474c2c15ba
patrick-rodgers <patrick.rodgers@microsoft.com>
2021-12-01 17:00:10 -0500

    remove clonedeep dep, dropped sp-addinhelpers package

--- packages/sp-addinhelpers/index.ts
+++
@@ -1,5 +0,0 @@
-// export * from "./sprequestexecutorclient.js";
-export * from "./sprestaddin.js";
-
-
-// TODO:: can we repurpose this to work for isolated webparts as a behavior in sp package
--- packages/sp-addinhelpers/~status.md
+++
@@ -1,2 +0,0 @@
-// TODO: MODULE STATUS: unreviewed
-// TODO:: test this with isolated web parts, if it works there like add-ins spend some time updating this and examples for isolated web parts.
