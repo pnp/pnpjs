a7de02bf27eee035b7d53a86631fb3b16c762a88
Patrick Rodgers <patrick.rodgers@microsoft.com>
2022-05-17 14:08:33 -0400

    updates for 3.3.1 addressing post-install

---
+++ packages/sp/post-install.cjs
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const projectRoot = process.env.INIT_CWD;
+const packageLoc = `${projectRoot}\\package.json`;
+const packageFile = fs.readFileSync(packageLoc, "utf8");
+const packageJSON = JSON.parse(packageFile);
+if (packageJSON.dependencies != null) {
+    const spfxVersion = packageJSON.dependencies["@microsoft/sp-core-library"];
+    if (spfxVersion != null) {
+        const spfxVersionFloat = parseFloat(spfxVersion);
+        if (spfxVersionFloat > 1.11 && spfxVersionFloat < 1.15) {
+            console.log("");
+            console.log("\x1b[43m%s\x1b[0m", " PnPjs WARNING ");
+            console.log("\x1b[33m%s\x1b[0m", "  The version of SPFx you are using requires an update to work with PnPjs. Please make sure to follow the getting started instructions to make the appropriate changes. ➡ https://pnp.github.io/pnpjs/getting-started/#spfx-version-1121-later");
+            console.log("");
+        }
+    }
+} else {
+    console.log("Package has no dependencies");
+}
