a065930f780b76a61945dae00d8c1e9f46a5ef27
Julie Turner <julie@julieturner.net>
2022-06-06 18:53:36 +0000

    Add Recycle Bin to Site object

--- packages/sp/recyclebin/index.ts
+++ packages/sp/recyclebin/index.ts
@@ -1,4 +1,5 @@
 import { addProp } from "@pnp/queryable";
+import { _Site } from "../sites/types.js";
 import { _Web } from "../webs/types.js";
 import { RecycleBin } from "./types.js";
 
@@ -9,3 +10,4 @@ export {
 } from "./types.js";
 
 addProp(_Web, "recycleBin", RecycleBin);
+addProp(_Site, "recycleBin", RecycleBin);
