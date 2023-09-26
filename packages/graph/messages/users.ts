6596f5a1280cc0b85416c0a705e6fe456f58a833
Beau Cameron <beau@Beaucameron.net>
2023-09-26 16:56:14 -0600

    Fixes Message mapping in Graph sendMail method

--- packages/graph/messages/users.ts
+++ packages/graph/messages/users.ts
@@ -2,19 +2,20 @@ import { addProp, body } from "@pnp/queryable";
 import { _User, User } from "../users/types.js";
 import { IMessages, Messages, IMailboxSettings, MailboxSettings, IMailFolders, MailFolders, IMessage } from "./types.js";
 import { graphPost } from "../operations.js";
+import { Message } from "@microsoft/microsoft-graph-types";
 
 declare module "../users/types" {
     interface _User {
         readonly messages: IMessages;
         readonly mailboxSettings: IMailboxSettings;
         readonly mailFolders: IMailFolders;
-        sendMail(message: IMessage): Promise<void>;
+        sendMail(message: Message): Promise<void>;
     }
     interface IUser {
         readonly messages: IMessages;
         readonly mailboxSettings: IMailboxSettings;
         readonly mailFolders: IMailFolders;
-        sendMail(message: IMessage): Promise<void>;
+        sendMail(message: Message): Promise<void>;
     }
 }
 
@@ -22,6 +23,6 @@ addProp(_User, "messages", Messages);
 addProp(_User, "mailboxSettings", MailboxSettings);
 addProp(_User, "mailFolders", MailFolders);
 
-_User.prototype.sendMail = function (this: _User, message: IMessage): Promise<void> {
+_User.prototype.sendMail = function (message: Message): Promise<void> {
     return graphPost(User(this, "sendMail"), body(message));
 };
