4029c008aa1a4616a57f820541a7461c19cdfafd
Beau Cameron <beau@Beaucameron.net>
2023-09-29 07:15:07 -0600

    Updating Graph Presence to include missing endpoints
    New documentation samples
    New tests added
    4 new endpoints added.

--- packages/graph/cloud-communications/types.ts
+++ packages/graph/cloud-communications/types.ts
@@ -1,5 +1,5 @@
 
-import { Presence as IUserPresence } from "@microsoft/microsoft-graph-types";
+import { Presence as IUserPresence  } from "@microsoft/microsoft-graph-types";
 import { _GraphCollection, graphInvokableFactory, _GraphInstance, graphPost } from "../graphqueryable.js";
 import { defaultPath } from "../decorators.js";
 import { body } from "@pnp/queryable";
@@ -9,7 +9,47 @@ import { body } from "@pnp/queryable";
  */
 
 @defaultPath("presence")
-export class _Presence extends _GraphInstance<IUserPresence> { }
+export class _Presence extends _GraphInstance<IUserPresence> {
+    /**
+     * Sets presence information for a user
+     *
+     * @param presence Presence object to set the state of a user's presence session
+     */
+    public async setPresence(presence: ISetPresenceOptions): Promise<ISetPresenceOptions> {
+
+        const postBody = { ...presence };
+        return graphPost(Presence(this, "setPresence"), body(postBody));
+    }
+
+    /**
+         * Clear application presence session of a user. If it is the user's only presence session, the user's presence will change to Offline/Offline.
+         *
+         * @param sessionId Id of the application to clear presence
+         */
+    public async clearPresence(sessionId: string): Promise<void> {
+
+        const postBody = { sessionId };
+        return graphPost(Presence(this, "clearPresence"), body(postBody));
+    }
+    /**
+     * Set the preferred availability and activity status for a user
+     *
+     * @param presence Presence object to set as preferred availbility and activity status of a user
+     */
+    public async setPreferredPresence(presence: IPresenceOptions): Promise<IPresenceOptions> {
+
+        const postBody = { ...presence };
+        return graphPost(Presence(this, "setUserPreferredPresence"), body(postBody));
+    }
+    /**
+     * Clears the preferred availability and activity status for a user
+     *
+     */
+    public async clearPreferredPresence(): Promise<void> {
+        return graphPost(Presence(this, "clearUserPreferredPresence"));
+    }
+
+}
 export interface IPresence extends _Presence { }
 export const Presence = graphInvokableFactory<IPresence>(_Presence);
 
@@ -23,9 +63,16 @@ export class _Communications extends _GraphCollection<IUserPresence[]> {
     public async getPresencesByUserId(ids: string[]): Promise<IUserPresence[]> {
 
         const postBody = { ids };
-        // return graphPost(<any>Search(this, "query"), body(request));
         return graphPost(Communications(this, "getPresencesByUserId"), body(postBody));
     }
 }
 export interface ICommunications extends _Communications { }
 export const Communications = graphInvokableFactory<ICommunications>(_Communications);
+
+export interface IPresenceOptions extends IUserPresence{
+    expirationDuration?: string;
+}
+
+export interface ISetPresenceOptions extends IPresenceOptions {
+    sessionId: string;
+}
---
+++ test/graph/cloud-communications.ts
@@ -0,0 +1,60 @@
+import { expect } from "chai";
+import "@pnp/graph/users";
+import "@pnp/graph/cloud-communications";
+import { stringIsNullOrEmpty } from "@pnp/core";
+
+
+describe("Cloud-Communications", function () {
+    let testUserId = "";
+    let sessionId = "";
+    // Ensure we have the data to test against
+    before(async function () {
+
+        if (!this.pnp.settings.enableWebTests || stringIsNullOrEmpty(this.pnp.settings.testUser)) {
+            this.skip();
+        }
+        testUserId =  (await this.pnp.graph.users.getById(this.pnp.settings.testUser.substring(this.pnp.settings.testUser.lastIndexOf("|") + 1))()).id;
+        sessionId = this.pnp.settings.graph.id;
+    });
+
+    it.skip("Get User Presence", async function () {
+        const presence = await this.pnp.graph.users.getById(testUserId).presence();
+        return expect(presence).is.not.null;
+    });
+
+    it.skip("Get Presence for Multiple Users", async function () {
+        const presence = await this.pnp.graph.communications.getPresencesByUserId([testUserId,testUserId]);
+        return expect(presence.length).is.equals(2);
+    });
+    
+    it("Set User Presence", async function () {
+        //617b607f-48d8-4431-b708-3c06c8a4d8c7
+        const presence = await this.pnp.graph.users.getById("617b607f-48d8-4431-b708-3c06c8a4d8c7").presence.setPresence({
+            availability: "Busy",
+            activity:"InACall",
+            sessionId: sessionId,
+            expirationDuration: "PT5M",
+        });
+        return expect(presence.availability).equals("Busy");
+    });
+
+    it("Clear User Presence", async function () {
+        const presence = await this.pnp.graph.users.getById('617b607f-48d8-4431-b708-3c06c8a4d8c7').presence.clearPresence(sessionId);
+        return true; 
+    });
+
+    it("Set User Preferred Presence", async function () {
+        const presence = await this.pnp.graph.users.getById(testUserId).presence.setPreferredPresence({
+            availability: "Available",
+            activity:"Available",
+            expirationDuration: "PT5M",
+        });
+        return expect(presence.availability).equals("Available");
+    });
+
+    it("Clear User Preferred Presence", async function () {
+        const presence = await this.pnp.graph.users.getById('617b607f-48d8-4431-b708-3c06c8a4d8c7').presence.clearPreferredPresence();
+        return true;
+    });
+
+});
