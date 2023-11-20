4c43d58cc5da8c0277c2157f71e4c94835b9254a
Dan Toft <Tanddant@gmail.com>
2023-11-20 18:29:12 +0100

    🔨 - OData filter inital prototype

--- packages/sp/spqueryable.ts
+++ packages/sp/spqueryable.ts
@@ -371,3 +371,174 @@ export interface IDeleteableWithETag {
      */
     delete(eTag?: string): Promise<void>;
 }
+
+
+
+export namespace OData {
+    enum FilterOperation {
+        Equals = "eq",
+        NotEquals = "ne",
+        GreaterThan = "gt",
+        GreaterThanOrEqualTo = "ge",
+        LessThan = "lt",
+        LessThanOrEqualTo = "le",
+        StartsWith = "startswith",
+        EndsWith = "endswith",
+        SubstringOf = "substringof"
+    }
+
+    enum FilterJoinOperator {
+        And = "and",
+        AndWithSpace = " and ",
+        Or = "or",
+        OrWithSpace = " or "
+    }
+
+    export const Where = <T = any>() => new ODataFilterClass<T>();
+
+    class BaseQueryable<T> {
+        protected query: string[] = [];
+
+        constructor(query: string[]) {
+            this.query = query;
+        }
+    }
+
+    class WhereClause<T> extends BaseQueryable<T> {
+        constructor(q: string[]) {
+            super(q);
+        }
+
+        public TextField(InternalName: keyof T): TextField<T> {
+            return new TextField<T>([...this.query, (InternalName as string)]);
+        }
+
+        public NumberField(InternalName: keyof T): NumberField<T> {
+            return new NumberField<T>([...this.query, (InternalName as string)]);
+        }
+
+        public DateField(InternalName: keyof T): DateField<T> {
+            return new DateField<T>([...this.query, (InternalName as string)]);
+        }
+
+        public BooleanField(InternalName: keyof T): BooleanField<T> {
+            return new BooleanField<T>([...this.query, (InternalName as string)]);
+        }
+    }
+
+    class ODataFilterClass<T> extends WhereClause<T>{
+        constructor() {
+            super([]);
+        }
+
+        public All(queries: BaseFilterCompareResult<T>[]): BaseFilterCompareResult<T> {
+            return new BaseFilterCompareResult<T>(["(", queries.map(x => x.ToString()).join(FilterJoinOperator.AndWithSpace), ")"]);
+        }
+
+        public Some(queries: BaseFilterCompareResult<T>[]): BaseFilterCompareResult<T> {
+            // This is pretty ugly, but avoids the space between the parenthesis and the first filter/last filter - I'm not sure if I like this, or the All one more, and then just living with the '( (' effect
+            return new BaseFilterCompareResult<T>([...queries.map((filter, index, arr) => `${index == 0 ? "(" : ""}${filter.ToString()}${arr.length-1 == index ? ")" : ""}`).join(FilterJoinOperator.OrWithSpace)]);
+        }
+    }
+
+    class BaseField<Ttype, Tinput> extends BaseQueryable<Ttype>{
+        constructor(q: string[]) {
+            super(q);
+        }
+
+        protected ToODataValue(value: Tinput): string {
+            return `'${value}'`;
+        }
+
+        public EqualTo(value: Tinput): BaseFilterCompareResult<Ttype> {
+            return new BaseFilterCompareResult<Ttype>([...this.query, FilterOperation.Equals, this.ToODataValue(value)]);
+        }
+
+        public NotEqualTo(value: Tinput): BaseFilterCompareResult<Ttype> {
+            return new BaseFilterCompareResult<Ttype>([...this.query, FilterOperation.NotEquals, this.ToODataValue(value)]);
+        }
+    }
+
+    class BaseComparableField<Tinput, Ttype> extends BaseField<Tinput, Ttype>{
+        constructor(q: string[]) {
+            super(q);
+        }
+
+        public GreaterThan(value: Ttype): BaseFilterCompareResult<Tinput> {
+            return new BaseFilterCompareResult<Tinput>([...this.query, FilterOperation.GreaterThan, this.ToODataValue(value)]);
+        }
+
+        public GreaterThanOrEqualTo(value: Ttype): BaseFilterCompareResult<Tinput> {
+            return new BaseFilterCompareResult<Tinput>([...this.query, FilterOperation.GreaterThanOrEqualTo, this.ToODataValue(value)]);
+        }
+
+        public LessThan(value: Ttype): BaseFilterCompareResult<Tinput> {
+            return new BaseFilterCompareResult<Tinput>([...this.query, FilterOperation.LessThan, this.ToODataValue(value)]);
+        }
+
+        public LessThanOrEqualTo(value: Ttype): BaseFilterCompareResult<Tinput> {
+            return new BaseFilterCompareResult<Tinput>([...this.query, FilterOperation.LessThanOrEqualTo, this.ToODataValue(value)]);
+        }
+    }
+
+    class TextField<T> extends BaseField<T, string>{
+        constructor(q: string[]) {
+            super(q);
+        }
+    }
+
+    class NumberField<T> extends BaseComparableField<T, number>{
+        constructor(q: string[]) {
+            super(q);
+        }
+
+        protected override ToODataValue(value: number): string {
+            return `${value}`;
+        }
+    }
+
+    class DateField<T> extends BaseComparableField<T, Date>{
+        constructor(q: string[]) {
+            super(q);
+        }
+
+        protected override ToODataValue(value: Date): string {
+            return `'${value.toISOString()}'`
+        }
+    }
+
+    class BooleanField<T> extends BaseField<T, boolean>{
+        constructor(q: string[]) {
+            super(q);
+        }
+
+        protected override ToODataValue(value: boolean): string {
+            return `${value == null ? null : value ? 1 : 0}`;
+        }
+    }
+
+
+    class BaseFilterCompareResult<T> extends BaseQueryable<T>{
+        constructor(q: string[]) {
+            super(q);
+        }
+
+        public Or(): FilterResult<T> {
+            return new FilterResult<T>(this.query, FilterJoinOperator.Or);
+        }
+
+        public And(): FilterResult<T> {
+            return new FilterResult<T>(this.query, FilterJoinOperator.And);
+        }
+
+        public ToString(): string {
+            return this.query.join(" ");
+        }
+    }
+
+    class FilterResult<T> extends WhereClause<T>{
+        constructor(currentQuery: string[], FilterJoinOperator: FilterJoinOperator) {
+            super([...currentQuery, FilterJoinOperator]);
+        }
+    }
+}
