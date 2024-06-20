34500c332cfb00b494156cfebf221409a57c185c
Jake Stanger <jake.stanger@core.co.uk>
2024-06-20 11:07:06 +0100

    feat(graph): add advanced query behaviour

--- test/graph/query-params.ts
+++ test/graph/query-params.ts
@@ -2,7 +2,7 @@ import { expect } from "chai";
 import { pnpTest } from "../pnp-test.js";
 import "@pnp/graph/groups";
 import "@pnp/graph/users";
-import { ConsistencyLevel } from "@pnp/graph/index.js";
+import { ConsistencyLevel, AdvancedQuery } from "@pnp/graph/index.js";
 
 describe("Graph Query Params", function () {
 
@@ -41,4 +41,27 @@ describe("Graph Query Params", function () {
 
         return expect(query()).to.eventually.be.fulfilled;
     }));
+
+    describe("AdvancedQuery", () => {
+        it("NOT groupTypes/any(c:c eq 'Unified')", pnpTest("d24d9b36-d5dc-4a6c-81fa-2e9a73911372", async function () {
+
+            const query = this.pnp.graph.groups.using(AdvancedQuery()).filter("NOT groupTypes/any(c:c eq 'Unified')");
+
+            return expect(query()).to.eventually.be.fulfilled;
+        }));
+
+        it("companyName ne null and NOT(companyName eq 'Microsoft')", pnpTest("33791988-de36-4a6d-88e1-23f6838236ac", async function () {
+
+            const query = this.pnp.graph.users.using(AdvancedQuery()).filter("companyName ne null and NOT(companyName eq 'Microsoft')");
+
+            return expect(query()).to.eventually.be.fulfilled;
+        }));
+
+        it("not(assignedLicenses/$count eq 0)", pnpTest("fe202c37-e10e-4b1c-b410-99cf059a491b", async function () {
+
+            const query = this.pnp.graph.users.using(AdvancedQuery()).filter("not(assignedLicenses/$count eq 0)");
+
+            return expect(query()).to.eventually.be.fulfilled;
+        }));
+    });
 });
