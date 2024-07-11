ab680aeaeaaf7fda9921abf5af73f49dc470b037
Roel <r.vanbaaren@link-it.nl>
2024-07-11 10:43:01 +0200

    Improved aliased parameter parsing

--- test/sp/alias.ts
+++ test/sp/alias.ts
@@ -6,6 +6,7 @@ import "@pnp/sp/files/web";
 import "@pnp/sp/files/folder";
 import "@pnp/sp/lists/web";
 import { combine } from "@pnp/core";
+import { SPQueryable } from "@pnp/sp";
 
 describe("Alias Parameters", function () {
 
@@ -26,6 +27,48 @@ describe("Alias Parameters", function () {
 
         await ler.list.rootFolder.folders.addUsingPath("MyTestFolder");
         await ler.list.rootFolder.files.addUsingPath("text.txt", "Some file content!");
+    });
+
+    it('Parameter parsing', function() {
+        /** Values to test */
+        const values = [
+            "value",
+            "value's",
+            "value with space",
+            "value with space' and apostrophe",
+            "ending with apostrophe'",
+            "'staring with apostrophe",
+            "'staring and ending with apostrophe'",
+            "with,' comma",
+        ];
+        /** Aliased parameters to test */
+        const tests = values.reduce<Record<string, Record<string, string>>>((obj, value)=>{
+            // Escape apostrophe in value
+            value = value.replace(/'/g, "''");
+
+            obj[`something('!@p1::${value}')`] = {'@p1': `'${value}'`};
+            obj[`something('!@p1::${value}','!@p2::${value}2')`] = {'@p1': `'${value}'`, '@p2': `'${value}2'`};
+            obj[`something('!@p1::${value}', param=value)`] = {'@p1': `'${value}'`};
+            obj[`something('!@p1::${value}', param=value, '!@p2::${value}2')`] = {'@p1': `'${value}'`, '@p2': `'${value}2'`};
+            obj[`something(param=value,'!@p1::${value}')`] = {'@p1': `'${value}'`};
+            obj[`something(param=value,'!@p1::${value}','!@p2::${value}2')`] = {'@p1': `'${value}'`, '@p2': `'${value}2'`};
+            obj[`something(param=value,'!@p1::${value}',param=value)`] = {'@p1': `'${value}'`};
+            obj[`something(param=value,'!@p1::${value}',param=value,'!@p2::${value}2')`] = {'@p1': `'${value}'`, '@p2': `'${value}2'`};
+            obj[`something(param='!@p1::${value}')`] = {'@p1': `'${value}'`};
+            obj[`something(param='!@p1::${value}',param2='!@p2::${value}2')`] = {'@p1': `'${value}'`, '@p2': `'${value}2'`};
+            return obj;
+        }, {});
+
+        // Test all aliased parameters
+        for(const [alias, params] of Object.entries(tests)) {
+            const requestUrl = SPQueryable(this.pnp.sp.web, alias).toRequestUrl();
+            const searchParams = Object.fromEntries(new URL(requestUrl).searchParams.entries());
+
+            for(const param in params) {
+                expect(searchParams, `Failed to parse "${alias}"`).to.have.property(param);
+                expect(searchParams[param], `Failed to parse "${alias}"`).to.equal(params[param]);
+            }
+        }
     });
 
     it("Folders", function () {
