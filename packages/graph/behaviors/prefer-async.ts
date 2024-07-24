05801be3bcecc9113310f532688a02ebea34ef6a
Chap <8684153+ChapC@users.noreply.github.com>
2024-07-24 14:20:07 +1000

    Manually call errorCheck Oops! I didn't read parseBinderWithErrorCheck properly and missed that it runs the impl function on all HTTP successes. I only want prefer-async to trigger on 202 Accepted. Updated my behaviour to manually add an errorCheck parse handler before my own.

--- packages/graph/behaviors/prefer-async.ts
+++ packages/graph/behaviors/prefer-async.ts
@@ -1,31 +1,35 @@
 import { TimelinePipe, delay } from "@pnp/core";
-import { InjectHeaders, parseBinderWithErrorCheck } from "@pnp/queryable";
+import { errorCheck, InjectHeaders } from "@pnp/queryable";
 import { GraphQueryable, _GraphQueryable, graphGet } from "../graphqueryable.js";
 import { RichLongRunningOperation } from "@microsoft/microsoft-graph-types";
 
 export function PreferAsync(pollIntervalMs: number = 25000, maxPolls: number = 4): TimelinePipe {
     return (instance: _GraphQueryable) => {
         instance.using(InjectHeaders({ "Prefer": "respond-async" }));
+        instance.on.parse(errorCheck);
+        instance.on.parse(async function (url: URL, response: Response, result: any) {
+            if (response.status === 202) {
+                const opLocation = response.headers.get("Location");
+                const opId = opLocation.split("/").at(-1);
 
-        const pollAsyncResult = async function (response) {
-            const opLocation = response.headers.get("Location");
-            const opId = opLocation.split("/").at(-1);
+                const statusQuery = GraphQueryable(instance, `operations/${opId}`);
+                for (let i = 0; i < maxPolls; i++) {
+                    await delay(pollIntervalMs);
 
-            const statusQuery = GraphQueryable(instance, `operations/${opId}`);
-            for (let i = 0; i < maxPolls; i++) {
-                await delay(pollIntervalMs);
-
-                const status = await statusQuery<RichLongRunningOperation>();
-                if (status.status === 'succeeded') {
-                    let resultEndpoint = status.resourceLocation.split("/").at(-1);
-                    return await graphGet(GraphQueryable(instance, resultEndpoint));
-                } else if (status.status === 'failed') {
-                    throw status.error;
+                    const status = await statusQuery<RichLongRunningOperation>();
+                    if (status.status === 'succeeded') {
+                        let resultEndpoint = status.resourceLocation.split("/").at(-1);
+                        result = await graphGet(GraphQueryable(instance, resultEndpoint));
+                    } else if (status.status === 'failed') {
+                        throw status.error;
+                    }
                 }
+                throw new Error(`Timed out waiting for async operation after ${pollIntervalMs * maxPolls}ms`);
             }
-            throw new Error(`Timed out waiting for async operation after ${pollIntervalMs * maxPolls}ms`);
-        };
 
-        return parseBinderWithErrorCheck(pollAsyncResult)(instance);
+            return [url, response, result];
+        });
+
+        return instance;
     }
 }
